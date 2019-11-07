160f6cf8e77ee58fa5f3f6c2ae654ea96e56ece0
paulorbsfilho <p4ulinho@outlook.com>
2019-11-07 15:35:42 -0300

    Update - Fix pk urls

--- comments/urls.py
+++ comments/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.ApiRoot.as_view(), name=views.ApiRoot.name),
     path('adress/', views.AddressList.as_view(), name=views.AddressList.name),
-    path('adress/<int:id>', views.AddressDetail.as_view(), name=views.AddressList.name),
+    path('adress/<int:pk>', views.AddressDetail.as_view(), name=views.AddressList.name),
     path('comments/', views.CommentList.as_view(), name=views.CommentList.name),
-    path('comments/<int:id>', views.CommentDetail.as_view(), name=views.CommentDetail.name),
+    path('comments/<int:pk>', views.CommentDetail.as_view(), name=views.CommentDetail.name),
     path('posts/', views.PostList.as_view(), name=views.PostList.name),
-    path('posts/<int:id>', views.PostDetail.as_view(), name=views.PostDetail.name),
+    path('posts/<int:pk>', views.PostDetail.as_view(), name=views.PostDetail.name),
     path('users/', views.UserList.as_view(), name=views.UserList.name),
-    path('users/<int:id>', views.UserDetail.as_view(), name=views.UserDetail.name),
+    path('users/<int:pk>', views.UserDetail.as_view(), name=views.UserDetail.name),
     path('database-upload/', views.DatabaseUpload.as_view(), name=views.DatabaseUpload.name),
 ]
